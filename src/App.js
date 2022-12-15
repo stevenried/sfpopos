c8d0c201424995d10f0d17530e5be570229cc4e1
Steven Ried <70533094+stevenried@users.noreply.github.com>
2022-12-14 23:31:44 -0500

    header component built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
+import Title from './components/Title'
+import logo from './logo.svg'
 
 function App() {
-  return <div></div>
+  return (
+    <div className="App">
+      <Title />
+      <img src={logo} alt="" />
+    </div>
+  )
 }
 
 export default App
