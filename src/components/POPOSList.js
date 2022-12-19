7864293790fca1e6b9d45af546a2b6ca5424c244
Steven Ried <70533094+stevenried@users.noreply.github.com>
2022-12-18 21:46:04 -0500

    Dynamic content and footer

--- src/components/POPOSList.js
+++ src/components/POPOSList.js
@@ -1,47 +1,22 @@
 import POPOSSpace from './POPOSSpace'
+import data from '../sfpopsos-data.json'
 import './POPOSList.css'
 
 function POPOSList() {
-  return (
-    <div className="list">
+  const spaces = data.map(({ title, address, images, likes, hours }) => {
+    return (
       <POPOSSpace
-        name="50 California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
-        likes={845}
+        key={title}
+        name={title}
+        address={address}
+        image={images[0]}
+        likes={likes}
+        hours={hours}
       />
-      <POPOSSpace
-        name="101 California Street"
-        address="101 California St."
-        image="101-california.jpg"
-        likes={2267}
-      />
-      <POPOSSpace
-        name="343 Sansome Roof"
-        address="343 Sansome St."
-        image="343-sansome-roof-garden.jpg"
-        likes={1345}
-      />
-      <POPOSSpace
-        name="525 Market Street Plaza"
-        address="525 Market St."
-        image="525-market-street-plaza.jpg"
-        likes={3826}
-      />
-      <POPOSSpace
-        name="555 California Street"
-        address="555 California St."
-        image="555-california.jpg"
-        likes={4237}
-      />
-      <POPOSSpace
-        name="100 Pine Street"
-        address="100 Pine St."
-        image="100-pine.jpg"
-        likes={928}
-      />
-    </div>
-  )
+    )
+  })
+
+  return <div className="list">{spaces}</div>
 }
 
 export default POPOSList
--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -5,14 +5,15 @@ function POPOSSpace(props) {
   const { name, image, address } = props
   return (
     <div className="space">
+      <h1>{name}</h1>
       <img
         src={`${process.env.PUBLIC_URL}images/${image}`}
         width="300"
         height="300"
         alt="Hello"
       />
-      <h1>{name}</h1>
       <div>{address}</div>
+      <div>{props.hours}</div>
       <div>Likes: {props.likes}</div>
     </div>
   )
